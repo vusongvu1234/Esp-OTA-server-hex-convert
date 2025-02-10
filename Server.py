7a5c77a805885308f22b4bc2060a3f49bb36c14a
vusongvu1234 <vuthiencuc@gmail.com>
2025-02-10 20:49:40 +0700

    Update Server.py

--- Server.py
+++ Server.py
@@ -31,4 +31,5 @@ def compile_arduino():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+   port = int(os.environ.get("PORT", 5000))  # Lấy PORT từ biến môi trường, mặc định là 5000
+   app.run(host='0.0.0.0', port=port)
